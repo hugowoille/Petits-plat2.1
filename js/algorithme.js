a1a83c7cf4871a40722b65f1860e97322c470896
hugowoille <hugo.woille@gmail.com>
2023-09-01 17:09:17 +0200

    ajout des ingrédient et du bouton voir plus

--- js/algorithme.js
+++ js/algorithme.js
@@ -23,21 +23,42 @@ function displayRecipients() {
 		const recipeDescription = document.createElement("p");
 		recipeDescription.textContent = recipe.description;
 		recipeDescription.classList.add("recette-description");
+		// RECETTE
+		const ingredientTitle = document.createElement("h3");
+		ingredientTitle.textContent = "INGREDIENT";
+		// Read more button
+		const readMoreButton = document.createElement("button");
+		readMoreButton.classList.add("readmore-button");
+		readMoreButton.textContent = "Voir plus";
+
+		readMoreButton.addEventListener("click", function () {
+			recipeDescription.style["-webkit-line-clamp"] = "unset";
+
+			readMoreButton.style.display = "none";
+		});
 		// ingrédients container
 		const ingredientsContainer = document.createElement("div");
 		ingredientsContainer.classList.add("ingredients-container");
 		const recipeIngredients = recipe.ingredients;
-		console.log("recipeIngredients:", recipeIngredients);
 
 		for (let i = 0; i < recipeIngredients.length; i++) {
 			const ingredient = recipeIngredients[i];
 			const ingredientsDiv = document.createElement("div");
-			ingredientsDiv.textContent = "hello";
+			ingredientsDiv.classList.add("ingredient");
 			ingredientsContainer.appendChild(ingredientsDiv);
+			const ingredientArray = Object.values(ingredient);
+			for (let i = 0; i < ingredientArray.length; i++) {
+				const nameOfIngredient = document.createElement("p");
+				nameOfIngredient.textContent = ingredientArray[i];
+				ingredientsDiv.appendChild(nameOfIngredient);
+			}
 		}
 		cardDescription.appendChild(recipeTitle);
 		cardDescription.appendChild(recetteTitle);
 		cardDescription.appendChild(recipeDescription);
+		cardDescription.appendChild(readMoreButton);
+		cardDescription.appendChild(ingredientTitle);
+
 		cardDescription.appendChild(ingredientsContainer);
 		card.appendChild(imgCard);
 		card.appendChild(cardDescription);
